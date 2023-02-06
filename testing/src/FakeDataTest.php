c2c2106366ede31c8ffbcad4bb4c224661342661
Jokebox90 <dev.jvagnier@gmail.com>
2023-02-06 15:59:47 +0100

    Ajoute la vérification des clés pour chaque article

--- testing/src/FakeDataTest.php
+++ testing/src/FakeDataTest.php
@@ -15,4 +15,28 @@ final class FakeDataTest extends TestCase
 
         $this->assertSame($nombreArticle, count($result));
     }
+
+    public function testGrandNombreArticle(): void
+    {
+        $nombreArticle = 5000;
+        $result = fake_data($nombreArticle);
+
+        $this->assertSame($nombreArticle, count($result));
+    }
+
+    public function testStructureDeChaqueArticle(): void
+    {
+        $nombreArticle = 5000;
+        $result = fake_data($nombreArticle);
+
+        $this->assertSame($nombreArticle, count($result));
+
+        for ($i = 0; $i < $nombreArticle; $i++) {
+            $article = $result[$i];
+            $this->assertArrayHasKey("title", $article);
+            $this->assertArrayHasKey("description", $article);
+            $this->assertArrayHasKey("price", $article);
+            $this->assertArrayHasKey("duration", $article);
+        }
+    }
 }
