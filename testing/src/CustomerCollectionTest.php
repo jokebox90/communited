99aa26c3f4dd75def96f5f9bb7e1781315abed6b
Jokebox90 <dev.jvagnier@gmail.com>
2023-02-07 11:06:20 +0100

    Ajoute une suite de test pour les clients

---
+++ testing/src/CustomerCollectionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+require_once('../staging/CustomerCollection.php');
+
+use PHPUnit\Framework\TestCase;
+
+final class CustomerCollectionTest extends TestCase
+{
+    public function testNombreCustomer(): void
+    {
+        $nombreCustomer = 5;
+        $collection = getCustomerCollection($nombreCustomer);
+
+        $this->assertSame($nombreCustomer, count($collection));
+    }
+
+    public function testGrandNombreCustomer(): void
+    {
+        $nombreCustomer = 5000;
+        $collection = getCustomerCollection($nombreCustomer);
+
+        $this->assertSame($nombreCustomer, count($collection));
+    }
+
+    public function testStructureDeChaqueCustomer(): void
+    {
+        $nombreCustomer = 5000;
+        $collection = getCustomerCollection($nombreCustomer);
+
+        for ($i = 0; $i < count($collection); $i++) {
+            $Customer = $collection[$i];
+            $this->assertIsArray($Customer);
+            $this->assertArrayHasKey("firstname", $Customer);
+            $this->assertArrayHasKey("lastname", $Customer);
+            $this->assertArrayHasKey("address", $Customer);
+            $this->assertArrayHasKey("phone", $Customer);
+            $this->assertArrayHasKey("email", $Customer);
+            $this->assertArrayHasKey("customer_id", $Customer);
+        }
+    }
+}
