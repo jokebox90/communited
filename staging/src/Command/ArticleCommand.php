a553eb4a3b9fa64477907262e2d060fe8ead575e
Jokebox90 <dev.jvagnier@gmail.com>
2023-02-08 15:08:35 +0100

    Adaptation des commandes aux nouvelles collections

--- staging/src/Command/ArticleCommand.php
+++ staging/src/Command/ArticleCommand.php
@@ -10,36 +10,57 @@ use App\Collection\ArticleCollection;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 
+#[AsCommand(
+    name: "app:article",
+    description: "Articles de la boutique en ligne.",
+)]
 final class ArticleCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('article');
-        $this->setDescription('Articles de la boutique en ligne.');
         $this->setHelp("Vous permet de gérer les articles de votre boutique.");
+        $this->addOption("limit", "l", InputOption::VALUE_OPTIONAL, "Nombre d'articles.", -1);
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $collection = ArticleCollection::getArticleCollection(5);
+        $io->section("Gestion des clients");
 
-        $io->table(
-            [
-                "title",
-                "description",
-                "price",
-                "duration",
-                "available",
-                "frequency",
-            ],
-            $collection
-        );
+        $limitValue = $input->getOption("limit");
+        $limit = ($limitValue > 0) ? $limitValue : 5;
+        $articles = new ArticleCollection();
+        $results = $articles->getAll($limit);
 
+        $io = new SymfonyStyle($input, $output);
+        for ($i = 0; $i < count($results); $i++) {
+            $io->section(sprintf("Article #%d", $i + 1));
+
+            $article = $results[$i];
+            $output->writeln(sprintf(" * unique_id:    %s", $article["unique_id"]));
+            $output->writeln(sprintf(" * title:        %s", $article["title"]));
+            $output->writeln(sprintf(" * description:  %s", $article["description"]));
+            $output->writeln(sprintf(" * available:    %d", $article["available"]));
+            $output->writeln(sprintf(" * tags:         %s", implode(", ", $article["tags"])));
+            $output->writeln(" * prices:");
+
+            for ($j = 0; $j < count($article["prices"]); $j++) {
+                $output->writeln(sprintf("\t * price #%d", $j + 1));
+
+                $price = $article["prices"][$j];
+                $output->writeln(sprintf("\t\t- amount:     %d € par semaine", $price["amount"]));
+                $output->writeln(sprintf("\t\t- frequency:  %s", $price["frequency"]));
+                $output->writeln(sprintf("\t\t- duration:   %d", $price["duration"]));
+            }
+            $output->writeln("");
+        }
+
+        $output->writeln("done.");
         return Command::SUCCESS;
     }
 }
--- staging/src/Command/CustomerCommand.php
+++ staging/src/Command/CustomerCommand.php
@@ -10,37 +10,53 @@ use App\Collection\CustomerCollection;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-
+#[AsCommand(
+    name: "app:customer",
+    description: "Clients de la boutique en ligne.",
+)]
 final class CustomerCommand extends Command
 {
+    const DEFAULT_CUSTOMER_LIMIT = 5;
+
     protected function configure()
     {
-        $this->setName('customer');
-        $this->setDescription('Clients de la boutique en ligne.');
         $this->setHelp("Vous permet de gérer les clients de votre boutique.");
+        $this->addOption("limit", "l", InputOption::VALUE_OPTIONAL, "Nombre de client.", 0);
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $collection = CustomerCollection::getCustomerCollection(5);
+        $io->section("Gestion des clients");
 
-        $io->table(
-            [
-                "firstname",
-                "lastname",
-                "address",
-                "phone",
-                "email",
-                "customer_id",
-                "birthdate",
-            ],
-            $collection
-        );
+        $limitValue = $input->getOption("limit");
+        $limit = ($limitValue > 0) ? $limitValue : CustomerCommand::DEFAULT_CUSTOMER_LIMIT;
+        $customers = new CustomerCollection();
+        $results = $customers->getAll($limit);
 
+        $io = new SymfonyStyle($input, $output);
+        for ($i = 0; $i < count($results); $i++) {
+            $io->section(sprintf("Client #%d", $i + 1));
+            $io->listing(
+                [
+                    sprintf("unique_id:   %s", $results[$i]["unique_id"]),
+                    sprintf("_sumùp_id:   %s", $results[$i]["_sumùp_id"]),
+                    sprintf("grade:       %s", $results[$i]["grade"]),
+                    sprintf("firstname:   %s", $results[$i]["firstname"]),
+                    sprintf("lastname:    %s", $results[$i]["lastname"]),
+                    sprintf("address:\r\n\t%s", implode("\r\n\t", array_values($results[$i]["address"]))),
+                    sprintf("phone:       %s", $results[$i]["phone"]),
+                    sprintf("email:       %s", $results[$i]["email"]),
+                    sprintf("birthdate:   %s", $results[$i]["birthdate"]),
+                ],
+            );
+        }
+
+        $output->writeln("done.");
         return Command::SUCCESS;
     }
 }
