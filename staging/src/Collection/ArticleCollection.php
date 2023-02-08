1b81536c86fcc029e1cd8e5c2cf8d8b3c68071ce
Jokebox90 <dev.jvagnier@gmail.com>
2023-02-08 15:07:36 +0100

    Remplace les méthodes statiques par des méthodes d'instance

--- staging/src/Collection/ArticleCollection.php
+++ staging/src/Collection/ArticleCollection.php
@@ -2,27 +2,75 @@
 
 namespace App\Collection;
 
+use Faker;
+
+
 final class ArticleCollection
 {
-    public static function getArticleCollection($limit)
-    {
-        $fake = \Faker\Factory::create('fr_FR');
 
-        $collection = array();
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = Faker\Factory::create('fr_FR');
+    }
+
+    public function getById(string $articleId)
+    {
+        return [
+            "unique_id"   => $articleId,
+            "title"       => $this->faker->sentence(5),
+            "description" => $this->faker->paragraph(),
+            "available"   => $this->faker->numberBetween(10, 50),
+            "prices"      => [
+                [
+                    "amount"    => $this->faker->randomElement([30, 60, 90, 120, 150]),
+                    "duration"  => $this->faker->numberBetween(1, 3),
+                    "frequency" => $this->faker->randomElement([
+                        "semaine",
+                        "mois",
+                        "trimestre",
+                        "annuel"
+                    ]),
+                ],
+                [
+                    "amount"    => $this->faker->randomElement([30, 60, 90, 120, 150]),
+                    "duration"  => $this->faker->numberBetween(1, 3),
+                    "frequency" => $this->faker->randomElement([
+                        "semaine",
+                        "mois",
+                        "trimestre",
+                        "annuel"
+                    ]),
+                ],
+                [
+                    "amount"    => $this->faker->randomElement([30, 60, 90, 120, 150]),
+                    "duration"  => $this->faker->numberBetween(1, 3),
+                    "frequency" => $this->faker->randomElement([
+                        "semaine",
+                        "mois",
+                        "trimestre",
+                        "annuel"
+                    ]),
+                ],
+            ],
+            "tags"        => [
+                $this->faker->randomElement(["un", "deux", "trois", "quatre"]),
+                $this->faker->randomElement(["cinq", "six", "sept", "huit"]),
+                $this->faker->randomElement(["neuf", "dix", "onze", "douze"]),
+                $this->faker->randomElement(["treize", "quatorze", "quinze", "seize"]),
+            ],
+        ];
+    }
+
+    public function getAll($limit)
+    {
+        $articleList = [];
 
         for ($i = 0; $i < $limit; $i++) {
-            $unArticleFakeIci = [
-                "title" => $fake->sentence(5),
-                "description" => $fake->paragraph(),
-                "price" => $fake->randomElement([30, 60, 90, 120, 150]),
-                "duration" => $fake->numberBetween(1, 5),
-                "available" => $fake->numberBetween(10, 50),
-                "frequency" => $fake->randomElement(["semaine", "mois", "trimestre", "semestre", "annuel"])
-            ];
-
-            array_push($collection, $unArticleFakeIci);
+            $articleList[] = $this->getById($this->faker->uuid());
         }
 
-        return $collection;
+        return $articleList;
     }
 }
--- staging/src/Collection/CustomerCollection.php
+++ staging/src/Collection/CustomerCollection.php
@@ -2,28 +2,59 @@
 
 namespace App\Collection;
 
-use DateTime;
+use Faker;
+
 
 final class CustomerCollection
 {
-    public static function getCustomerCollection($limit)
+    const GRADE_CONTACT    = "contact";
+    const GRADE_VISITOR    = "visiteur";
+    const GRADE_MEMBER     = "membre";
+    const GRADE_AMBASSADOR = "ambassadeur";
+    const GRADE_FOUNDER    = "fondateur";
+    const GRADES = [
+        self::GRADE_CONTACT,
+        self::GRADE_VISITOR,
+        self::GRADE_MEMBER,
+        self::GRADE_AMBASSADOR,
+        self::GRADE_FOUNDER,
+    ];
+
+    private $faker;
+
+    public function __construct()
     {
-        $fake = \Faker\Factory::create('fr_FR');
-        $customer = array();
+        $this->faker = Faker\Factory::create('fr_FR');
+    }
+
+    public function getById(string $uniqueId)
+    {
+        return [
+            "_sumùp_id" => $uniqueId,
+            "unique_id" => $this->faker->uuid(),
+            "firstname" => $this->faker->firstName(),
+            "lastname"  => $this->faker->lastName(),
+            "address"   => [
+                "street"     => $this->faker->streetAddress(),
+                "postalCode" => $this->faker->postCode(),
+                "city"       => $this->faker->city(),
+                "country"    => "France",
+            ],
+            "phone"     => $this->faker->phoneNumber(),
+            "email"     => $this->faker->safeEmail(),
+            "birthdate" => $this->faker->dateTimeBetween('-99 year', '-13 years')->format("Y-m-d"),
+            "grade"     => $this->faker->randomElement(self::GRADES),
+        ];
+    }
+
+    public function getAll($limit)
+    {
+        $customerList = array();
+
         for ($i = 0; $i < $limit; $i++) {
-            $fake_customer = [
-                "firstname"  => $fake->firstName(),
-                "lastname"   => $fake->lastName(),
-                "address"    => $fake->address(),
-                "phone"      => $fake->mobileNumber(),
-                "email"      => $fake->safeEmail(),
-                "customer_id" => $fake->uuid(),
-                "birthdate"  => $fake->dateTimeBetween('-30 year', '-15 years')->format(DateTime::ISO8601),
-            ];
-            array_push($customer, $fake_customer);
+            $customerList[] = $this->getById($this->faker->uuid());
         }
-        return $customer;
+
+        return $customerList;
     }
 }
-
-// print_r(CustomerCollection::getCustomerCollection(5));
